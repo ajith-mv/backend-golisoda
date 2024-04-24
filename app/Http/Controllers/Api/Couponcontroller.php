c6e26e28d8403e6d0995edcf057e06196d39815c
ajithmv <80975119+ajith-mv@users.noreply.github.com>
2024-04-24 15:23:36 +0530

    Update Couponcontroller.php

--- app/Http/Controllers/Api/Couponcontroller.php
+++ app/Http/Controllers/Api/Couponcontroller.php
@@ -472,12 +472,12 @@ $tax_percentage = 0;
                     if($type !='remove' && isset($citems->coupon_id)){
                         // $price=$items->strike_price /(1+$tax_data);
                         $price_with_tax         =$items->strike_price;
-                        $citems->sub_total=round($price * $citems->quantity);
+                        $citems->sub_total=round($price_with_tax * $citems->quantity);
                          $citems->update();
                     }else{
                     //   $price=$items->mrp /(1+$tax_data);
                         $price_with_tax         = $items->mrp;
-                        $citems->sub_total=round($price * $citems->quantity);
+                        $citems->sub_total=round($price_with_tax * $citems->quantity);
                         $citems->update();
                         
                     }
