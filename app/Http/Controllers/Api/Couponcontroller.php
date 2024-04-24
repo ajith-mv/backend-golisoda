0ea64e9154306ba8dc1fae63b5fa958ab5a240cf
ajithmv <80975119+ajith-mv@users.noreply.github.com>
2024-04-24 14:56:47 +0530

    Update Couponcontroller.php

--- app/Http/Controllers/Api/Couponcontroller.php
+++ app/Http/Controllers/Api/Couponcontroller.php
@@ -311,6 +311,7 @@ case '5':
                                 ->where('carts.customer_id', $customer_id)
                                 //->groupBy('carts.product_id')
                                 ->first();
+                                dd($checkCartData);
             if(!$checkCartData){
             return '';
             }
