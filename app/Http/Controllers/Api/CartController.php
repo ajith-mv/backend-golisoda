244dc4ff34beeb1b1354468f02976c90214f4351
shimymelbin <dalbin@capecomsolutions.com>
2024-05-16 16:07:29 +0530

    debug

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -805,7 +805,7 @@ class CartController extends Controller
                             }
                         }
                     }
-                    if (isset($selected_value)) {
+                    if (isset($selected_value) && !empty($selected_value)) {
                         $items->strike_price = $items->strike_price + $total_variation_amount;
                         $items->mrp = $items->strike_price + $total_variation_amount;
                         $items->discount_percentage = 0;
