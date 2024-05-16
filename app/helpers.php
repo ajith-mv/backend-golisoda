f634e12ade7ddb4f1476b28a23f7be6c08ef8227
shimymelbin <dalbin@capecomsolutions.com>
2024-05-16 14:13:42 +0530

    debug fix

--- app/helpers.php
+++ app/helpers.php
@@ -336,7 +336,6 @@ function getProductApiData($product_data, $customer_id = '', $variation_option_i
     if (isset($default_value) && !empty($default_value)) {
         $product_data->mrp = $product_data->strike_price;
         $product_data->strike_price = $product_data->strike_price + $total_variation_amount;
-        dd($product_data);
     }
     $price_data = getProductPrices($product_data);
 
