82dd38a9897927ac6f518e6691ab87bea5833b00
shimymelbin <dalbin@capecomsolutions.com>
2024-05-19 21:19:47 +0530

    debug cart data

--- app/Http/Controllers/Api/Couponcontroller.php
+++ app/Http/Controllers/Api/Couponcontroller.php
@@ -266,14 +266,18 @@ class Couponcontroller extends Controller
                                 ->where('carts.customer_id', $customer_id)
                                 // ->groupBy('carts.product_id')
                                 ->pluck('id')->toArray();
+
+                            log::info($cartCountNew);
                             $product_info = Product::find($checkCartData->product_id);
 
 
                             $cart_variation_option = CartProductVariationOption::where('product_id', $checkCartData->product_id)->whereIn('cart_id', $cartCountNew)->groupBy('product_id')->selectRaw("SUM(amount) AS total_amount")->first();
+                            log::info($cart_variation_option);
+
                             if (isset($cart_variation_option) && !empty($cart_variation_option)) {
                                 $product_info->strike_price = $product_info->strike_price + $cart_variation_option->total_amount;
                             }
-                            
+
                             $checkCartData->sub_total = round($product_info->strike_price * $checkCartData->quantity);
                             $checkCartData->update();
                             if (isset($checkCartData) && !empty($checkCartData)) {
