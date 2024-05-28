ae7bcd8aeca880743af706ee1bc64cbd594d1063
shimymelbin <dalbin@capecomsolutions.com>
2024-05-28 22:24:52 +0530

    debug

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -874,12 +874,12 @@ class CartController extends Controller
                     $category               = $items->productCategory;
                     if (isset($citems->coupon_id)) {
                         // $price=$items->strike_price /(1+$tax_data);
-                        $price_with_tax         = $items->strike_price;
+                        $price_with_tax         = $citems->price;
                         $citems->sub_total = round($price_with_tax * $citems->quantity);
                         $citems->save();
                     } else {
                         // $price=$items->mrp /(1+$tax_data);
-                        $price_with_tax         = $items->mrp;
+                        $price_with_tax         = $citems->price;
                         $citems->sub_total = round($price_with_tax * $citems->quantity);
                         Log::info("subtotal cart list 2: ". $citems->sub_total);
                         $citems->save();
