5db71f11fc558c7da177f3a7ffd2553beb4472c3
shimymelbin <dalbin@capecomsolutions.com>
2024-05-28 11:47:06 +0530

    remove discount percentage for variation amount that has no discount

--- app/Http/Controllers/Api/Couponcontroller.php
+++ app/Http/Controllers/Api/Couponcontroller.php
@@ -534,7 +534,7 @@ class Couponcontroller extends Controller
                 if (isset($selected_value)) {
                     $items->strike_price = $items->strike_price + $total_variation_amount;
                     $items->mrp = $items->strike_price + $total_variation_amount;
-                    // $items->discount_percentage = 0;
+                    $items->discount_percentage = ($total_discount_amount > 0) ? $items->discount_percentage : 0;
                 }
                 $category               = $items->productCategory;
                 if ($type != 'remove' && isset($citems->coupon_id)) {
