cd7a1f2286d2d932ed2c8fdf4e53479979662f34
shimymelbin <dalbin@capecomsolutions.com>
2024-05-28 17:40:45 +0530

    change query of product coupon discount

--- app/Http/Controllers/Api/Couponcontroller.php
+++ app/Http/Controllers/Api/Couponcontroller.php
@@ -66,8 +66,7 @@ class Couponcontroller extends Controller
                             if (isset($coupon->couponProducts) && !empty($coupon->couponProducts)) {
                                 $couponApplied['coupon_type'] = array('discount_type' => $coupon->calculate_type, 'discount_value' => $coupon->calculate_value);
                                 foreach ($coupon->couponProducts as $items) {
-                                    $cartCount = Cart::where('customer_id', $customer_id)->where('product_id', $items->product_id)->selectRaw("gbs_carts.*, SUM(quantity) as quantity")->groupBy('product_id')->get();
-                                    dd($cartCount);
+                                    $cartCount = Cart::where('customer_id', $customer_id)->where('product_id', $items->product_id)->selectRaw("gbs_carts.*, SUM(quantity) as quantity, SUM(sub_total) as sub_total")->groupBy('product_id')->first();
                                     if (!isset($cartCount)) {
                                         $response['status'] = 'error';
                                         $response['message'] = 'Coupon not applicable';
@@ -82,7 +81,7 @@ class Couponcontroller extends Controller
                                     if (isset($cart_variation_option) && !empty($cart_variation_option)) {
                                         $product_info->strike_price = $product_info->strike_price + $cart_variation_option->total_amount;
                                     }
-                                    $cartCount->sub_total = round($product_info->strike_price * $cartCount->quantity);
+                                    // $cartCount->sub_total = round($product_info->strike_price * $cartCount->quantity);
                                     $cartCount->update();
                                     if ($cartCount) {
                                         if ($cartCount->sub_total >= $coupon->minimum_order_value) {
