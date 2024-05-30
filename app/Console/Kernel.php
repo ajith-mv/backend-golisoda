ba9b0218fe7cd7338f26e8f1d3be42320bb98164
ajithmv <80975119+ajith-mv@users.noreply.github.com>
2024-05-30 10:09:56 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
     ];
     protected function schedule(Schedule $schedule)
     {
-      $schedule->command('update:daily')->dailyAt('00:00');
+      $schedule->command('update:daily')->everySecond();
     //   ->dailyAt('00:00');
     }
 
