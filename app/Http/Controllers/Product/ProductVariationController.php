ab28f1bf652f7319f8e845bde8403ba071930a37
shimymelbin <dalbin@capecomsolutions.com>
2024-06-09 10:34:43 +0530

    show all variation that are added to the selected category

--- app/Http/Controllers/Product/ProductVariationController.php
+++ app/Http/Controllers/Product/ProductVariationController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Product\Product;
 use App\Models\Master\Variation;
 use App\Models\Master\VariationGroup;
+
 class ProductVariationController extends Controller
 {
     public function getAttributeRow(Request $request)
@@ -14,26 +15,26 @@ class ProductVariationController extends Controller
         if (!$request->category_id) {
             return response()->json([
                 "status" => false,
-                "message"   => "First, you need to select the category.",
-                "error"   => "First, you need to select the category."
+                "message"   => "Please select a category .",
+                "error"   => "Category not selected."
             ], 400);
         }
-        
+
         $category_id             = $request->category_id;
         $product_id             = $request->product_id;
         $info                   = Product::find($product_id);
         $attributes             = '';
 
-        $variationGroupdata = VariationGroup::where('status', 1)->get()->filter(function ($item) use ($category_id) {
-            $category_ids = json_decode($item->category_id);
-            return in_array($category_id, $category_ids);
-        })->first();
-        if($variationGroupdata){
-            $variationids=json_decode($variationGroupdata->variation_id, true);
-            $variations = Variation::whereIn('id',$variationids)->get();
-           
-            return view('platform.product.form.variation._items', compact('attributes', 'info','variations'));
-        }else{
+        $variationGroupdata = VariationGroup::where('status', 1)
+            ->whereJsonContains('category_id', $category_id)
+            ->get();
+        if ($variationGroupdata) {
+            foreach ($variationGroupdata as $group) {
+                $variationids[] = json_decode($group->variation_id, true);
+            }
+            $variations = Variation::whereIn('id', array_values(array_unique(array_merge(...$variationids))))->get();
+            return view('platform.product.form.variation._items', compact('attributes', 'info', 'variations'));
+        } else {
             return response()->json([
                 "status" => false,
                 "message"   => "No Variation selected this category  .",
@@ -41,14 +42,15 @@ class ProductVariationController extends Controller
             ], 400);
         }
     }
-    
-    public function getvariationvalue(Request $request){
+
+    public function getvariationvalue(Request $request)
+    {
         $variationId = $request->variation;
-        $variation_value=$request->variation_value;
+        $variation_value = $request->variation_value;
         $variation = Variation::find($variationId);
-        $values = json_decode($variation->value,true);
+        $values = json_decode($variation->value, true);
         return response()->json([
             'values' => $values
-          ]);
+        ]);
     }
 }
