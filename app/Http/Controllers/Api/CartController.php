340fdc7a06195d77b011eadbcb283456299f4ece
shimymelbin <dalbin@capecomsolutions.com>
2024-06-20 20:25:45 +0530

    debug

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -1327,6 +1327,7 @@ class CartController extends Controller
             }
         }
         $uniqueIsFree = array_unique($is_free);
+        dd($uniqueIsFree);
         if (count($uniqueIsFree) === 1 && reset($uniqueIsFree) == 1) {
             $chargeData = ['shipping_title' => "Free Shipping1", 'is_free' => 1, 'charges' => 0];
 
