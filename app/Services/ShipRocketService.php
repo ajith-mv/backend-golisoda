9a116a2ab31d41a5ba83ebb905c7b01fd68b7e4a
shimy-pixel <134940044+shimy-pixel@users.noreply.github.com>
2024-07-16 05:31:02 +0530

    syntax fix

--- app/Services/ShipRocketService.php
+++ app/Services/ShipRocketService.php
@@ -247,7 +247,7 @@ class ShipRocketService
                                     $orderItems = $data['cartItemsarr'];
                                     $cart_total = $data['cartTotal'];
                                     $measure_ment = $data['measurement'];
-                            
+
                                     $params = $this->getRequestForCreateOrderApi(
                                         $data['citems'],
                                         $data['cartShipAddress'],
@@ -258,30 +258,31 @@ class ShipRocketService
                                         $data['total_weight']
                                     );
                                     $createResponse = $this->createOrder($params);
-                                if (isset($createResponse) && !empty($createResponse['order_id'])) {
-                                    // $shipping_amount = $shipping_amount + $this->getShippingCharges($createResponse['order_id'], $createOrderData[$brandId]['measurement'], $pickup_post_code, $delivery_post_code);
-                                    $shiprocket_shipping_charges = $this->getShippingCharges($createResponse['order_id'], $measure_ment, $pickup_post_code, $delivery_post_code);
-                                    $shipping_amount = $shipping_amount + $shiprocket_shipping_charges;
-                                    if (isset($shiprocket_shipping_charges) && !empty($shiprocket_shipping_charges) && ($shiprocket_shipping_charges != 0)) {
-                                        $shipment['shiprocket_amount'] = $shiprocket_shipping_charges;
-                                        $shipment['shipping_amount'] = $shiprocket_shipping_charges;
-                                        $shipment['shipping_type'] = 'standard_shipping';
-                                        $shipment['shipping_id'] = 2;
-                                    } else {
-                                        $flat_shipping = getVolumeMetricCalculation($data['measurement']['length'], $data['measurement']['breadth'], $data['measurement']['height']);
-                                        $shipment['shipping_amount'] = $flat_shipping * 50;
-                                        $shipment['shipping_type'] = 'flat_shipping';
-                                        $shipment['shipping_id'] = 3;
+                                    if (isset($createResponse) && !empty($createResponse['order_id'])) {
+                                        // $shipping_amount = $shipping_amount + $this->getShippingCharges($createResponse['order_id'], $createOrderData[$brandId]['measurement'], $pickup_post_code, $delivery_post_code);
+                                        $shiprocket_shipping_charges = $this->getShippingCharges($createResponse['order_id'], $measure_ment, $pickup_post_code, $delivery_post_code);
+                                        $shipping_amount = $shipping_amount + $shiprocket_shipping_charges;
+                                        if (isset($shiprocket_shipping_charges) && !empty($shiprocket_shipping_charges) && ($shiprocket_shipping_charges != 0)) {
+                                            $shipment['shiprocket_amount'] = $shiprocket_shipping_charges;
+                                            $shipment['shipping_amount'] = $shiprocket_shipping_charges;
+                                            $shipment['shipping_type'] = 'standard_shipping';
+                                            $shipment['shipping_id'] = 2;
+                                        } else {
+                                            $flat_shipping = getVolumeMetricCalculation($data['measurement']['length'], $data['measurement']['breadth'], $data['measurement']['height']);
+                                            $shipment['shipping_amount'] = $flat_shipping * 50;
+                                            $shipment['shipping_type'] = 'flat_shipping';
+                                            $shipment['shipping_id'] = 3;
+                                        }
+                                        if (isset($brand_data) && ($brand_data->is_free_shipping == 1)) {
+                                            $shipment['shipping_amount'] = 0;
+                                            $shipment['shipping_type'] = 'free_shipping';
+                                            $shipment['shipping_id'] = 1;
+                                        }
+                                        CartShipment::where('cart_id', $data['citems']->id)->delete();
+                                        $shipment['cart_id'] = $data['citems']->id;
+                                        $shipment['brand_id'] = $brandId;
+                                        CartShipment::create($shipment);
                                     }
-                                    if (isset($brand_data) && ($brand_data->is_free_shipping == 1)) {
-                                        $shipment['shipping_amount'] = 0;
-                                        $shipment['shipping_type'] = 'free_shipping';
-                                        $shipment['shipping_id'] = 1;
-                                    }
-                                    CartShipment::where('cart_id', $data['citems']->id)->delete();
-                                    $shipment['cart_id'] = $data['citems']->id;
-                                    $shipment['brand_id'] = $brandId;
-                                    CartShipment::create($shipment);
                                 }
                             }
                             // foreach ($request_params as $params) {
