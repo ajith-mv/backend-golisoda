e0b849fb775288765c98d75131833177c1f53876
shimy-pixel <134940044+shimy-pixel@users.noreply.github.com>
2024-07-29 10:48:52 +0530

    debug

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -1301,9 +1301,9 @@ class CartController extends Controller
                 } else {
                     $coupon_amount = $coupon_data->coupon_amount;
                 }
-                // if ($coupon_amount < 1) {
-                //     $is_coupon = 0;
-                // }
+                if ($coupon_amount < 1) {
+                    $is_coupon = 0;
+                }
             }
             $tmp['cart_total']          = array(
                 'total' => number_format(round($grand_total), 2),
